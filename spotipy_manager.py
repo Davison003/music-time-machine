495fb19fe0bf518d4cd685d617b045b65f51c123
Davi <daviregonatti@gmail.com>
2024-09-11 00:14:49 -0300

    refactor: changed file functions to a single manager class with methods

--- spotipy_manager.py
+++ spotipy_manager.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
 load_dotenv("./.env")
-SPOTIFY_ENDPOINT = os.getenv("SPOTIFY_ENDPOINT")
+
 SPOTIFY_CLIENTID = os.getenv("CLIENT_ID")
 SPOTIFY_SECRET = os.getenv("CLIENT_SECRET")
 
@@ -9,35 +9,43 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 from spotipy.oauth2 import SpotifyClientCredentials
 
-def spotipy_oauth(scope):
-    return spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope,
-                                                     client_id=SPOTIFY_CLIENTID,
-                                                     client_secret=SPOTIFY_SECRET,
-                                                     redirect_uri="http://example.com"))
+class SpotipyManager:
+    def __init__(self, music_date):
+        self.client_id = SPOTIFY_CLIENTID
+        self.client_secret = SPOTIFY_SECRET
+        self.redirect = "http://example.com"
+        self.user_date = music_date
 
-def spotipy_cli_cred():
-    return spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(
-        client_id=SPOTIFY_CLIENTID,
-        client_secret=SPOTIFY_SECRET))
+    def spotipy_oauth(self, scope):
+        return spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope,
+                                                         client_id=self.client_id,
+                                                         client_secret=self.client_secret,
+                                                         redirect_uri=self.redirect))
 
-def create_playlist(music_date):
-    scope = "playlist-modify-public"
-    sp = spotipy_oauth(scope)
-    user_id = sp.me()['id']
-    sp.user_playlist_create(user_id, f'25 hits from {music_date}',
-                            description=f'Playlist created via Python script, '
-                                        f'contains the "best" hits from {music_date}')
+    def spotipy_cli_cred(self):
+        return spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(
+            client_id=self.client_id,
+            client_secret=self.client_secret))
 
-
-def add_songs_playlist(tracks, music_date):
-    scope = 'playlist-modify-public'
-    sp = spotipy_oauth(scope)
-    playlist = [pl['uri'] for pl in sp.current_user_playlists()['items']
-                if pl['name'] == f'25 hits from {music_date}']
-    sp.playlist_add_items(playlist[0], tracks)
+    def search_track(self, song_details):
+        sp = self.spotipy_cli_cred()
+        result = sp.search(song_details)
+        return result["tracks"]["items"][0]["uri"]
 
 
-def search_track(song_details):
-    sp = spotipy_cli_cred()
-    result = sp.search(song_details)
-    return result["tracks"]["items"][0]["uri"]
+    def create_playlist(self):
+        scope = "playlist-modify-public"
+        sp = self.spotipy_oauth(scope)
+        user_id = sp.me()['id']
+        sp.user_playlist_create(user_id, f'25 hits from {self.user_date}',
+                                description=f'Playlist created via Python script, '
+                                            f'contains the "best" hits from {self.user_date}')
+
+    def add_songs_playlist(self, tracks):
+        scope = 'playlist-modify-public'
+        sp = self.spotipy_oauth(scope)
+        playlist = [pl['uri'] for pl in sp.current_user_playlists()['items']
+                    if pl['name'] == f'25 hits from {self.user_date}']
+        sp.playlist_add_items(playlist[0], tracks)
+
+
